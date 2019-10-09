f3ca776b19cb43e5e8546e526fd207ebd3a4ba1c
Kemsara <ethan.zachary.punter@gmail.com>
2019-10-10 07:21:09 +1300

    Verb Major GUI Update

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -19,30 +19,19 @@ TokyoRe_verbAudioProcessorEditor::TokyoRe_verbAudioProcessorEditor (TokyoRe_verb
     // Make sure that before the constructor has finished, you've set the
     // editor's size to whatever you need it to be.
     setOpaque(true);
-    setSize (540, 450);
+    setSize (700, 394);
     
-    filterCutoffDial.setTextBoxStyle(Slider::TextEntryBoxPosition::TextBoxAbove, true, 55, 30);
-    filterCutoffDial.setColour(Slider::ColourIds::textBoxTextColourId, Colours::black);
-    filterCutoffDial.setColour(Slider::ColourIds::textBoxOutlineColourId, Colour());
-    filterCutoffDial.setSliderStyle(Slider::SliderStyle::Rotary);
-    filterCutoffDial.setNumDecimalPlacesToDisplay(0);
-    filterCutoffDial.setSize(150, 100);
-    //filterCutoffDial.setRange(20.0, 20000.0);
-    filterCutoffDial.setTextValueSuffix("hz");
-    //filterCutoffDial.setColour(Slider::ColourIds::rotarySliderFillColourId, Colours::yellowgreen);
+    filterCutoffDial.setTextBoxStyle(Slider::NoTextBox, false, 0, 0);
+    filterCutoffDial.setSliderStyle(Slider::SliderStyle::RotaryVerticalDrag);
+    filterCutoffDial.setSize(80, 80);
     
     // dont change these two
     filterCutoffDial.addListener(this);
     addAndMakeVisible(filterCutoffDial);
     
-    filterResDial.setTextBoxStyle(Slider::TextEntryBoxPosition::TextBoxAbove, true, 50,30);
-    filterResDial.setColour(Slider::ColourIds::textBoxOutlineColourId, Colour());
-    filterResDial.setSliderStyle(Slider::SliderStyle::Rotary);
-    filterResDial.setSize(150, 100);
-    //filterResDial.setRange(1.0, 5.0);
-    filterResDial.setTextValueSuffix("Q");
-    //filterResDial.setColour(Slider::ColourIds::rotarySliderFillColourId, Colours::yellowgreen);
-    filterResDial.setColour(Slider::ColourIds::textBoxTextColourId, Colours::black);
+    filterResDial.setTextBoxStyle(Slider::NoTextBox, false, 0, 0);
+    filterResDial.setSliderStyle(Slider::SliderStyle::RotaryVerticalDrag);
+    filterResDial.setSize(80, 80);
     
     // dont change these two
     filterResDial.addListener(this);
@@ -52,88 +41,44 @@ TokyoRe_verbAudioProcessorEditor::TokyoRe_verbAudioProcessorEditor (TokyoRe_verb
     filterCutoffValue = new AudioProcessorValueTreeState::SliderAttachment (processor.mState, "cutoff", filterCutoffDial);
     filterResValue = new AudioProcessorValueTreeState::SliderAttachment (processor.mState, "resonance", filterResDial);
     
-    filterCutoffDial.setSkewFactorFromMidPoint(1000.0f);
-    
-    filterCutoffDial.setLookAndFeel(&otherLookAndFeel2);
-    filterResDial.setLookAndFeel(&otherLookAndFeel2);
+    filterCutoffDial.setLookAndFeel(&otherLookAndFeel);
+    filterResDial.setLookAndFeel(&otherLookAndFeel);
     
     //addAndMakeVisible(filterCutoffLabel);
-    // filterCutoffLabel.setText("Cutoff Freq", dontSendNotification);
     mixLabel.attachToComponent(&filterCutoffDial, false);
     
     addAndMakeVisible(filterResLabel);
-    //filterResLabel.setText("Q", dontSendNotification);
     filterResLabel.attachToComponent(&filterResDial, false);
     
-    /*reverbDryDial.setTextBoxStyle(Slider::TextEntryBoxPosition::TextBoxAbove, true, 55, 30);
-    reverbDryDial.setColour(Slider::ColourIds::textBoxTextColourId, Colours::black);
-    reverbDryDial.setColour(Slider::ColourIds::textBoxOutlineColourId, Colour());
-    reverbDryDial.setSliderStyle(Slider::SliderStyle::Rotary);
-    reverbDryDial.setNumDecimalPlacesToDisplay(0);
-    reverbDryDial.setSize(150, 100);
-    reverbDryDial.setTextValueSuffix("%");
-
-    // dont change these two
-    reverbDryDial.addListener(this);
-    addAndMakeVisible(reverbDryDial);
-    
-    reverbWetDial.setTextBoxStyle(Slider::TextEntryBoxPosition::TextBoxAbove, true, 55, 30);
-    reverbWetDial.setColour(Slider::ColourIds::textBoxTextColourId, Colours::black);
-    reverbWetDial.setColour(Slider::ColourIds::textBoxOutlineColourId, Colour());
-    reverbWetDial.setSliderStyle(Slider::SliderStyle::Rotary);
-    reverbWetDial.setNumDecimalPlacesToDisplay(0);
-    reverbWetDial.setSize(150, 100);
-    reverbWetDial.setTextValueSuffix("%");
-    
-    // dont change these two
-    reverbWetDial.addListener(this);
-    addAndMakeVisible(reverbWetDial);*/
-    
-    reverbMixDial.setTextBoxStyle(Slider::TextEntryBoxPosition::TextBoxAbove, true, 55, 30);
-    reverbMixDial.setColour(Slider::ColourIds::textBoxTextColourId, Colours::black);
-    reverbMixDial.setColour(Slider::ColourIds::textBoxOutlineColourId, Colour());
-    reverbMixDial.setSliderStyle(Slider::SliderStyle::Rotary);
-    reverbMixDial.setNumDecimalPlacesToDisplay(0);
-    reverbMixDial.setSize(150, 100);
-    reverbMixDial.setTextValueSuffix("%");
+    reverbMixDial.setTextBoxStyle(Slider::NoTextBox, false, 0, 0);
+    reverbMixDial.setSliderStyle(Slider::SliderStyle::RotaryVerticalDrag);
+    reverbMixDial.setSize(80, 80);
     
     // dont change these two
     reverbMixDial.addListener(this);
     addAndMakeVisible(reverbMixDial);
     
-    reverbRoomDial.setTextBoxStyle(Slider::TextEntryBoxPosition::TextBoxAbove, true, 55, 30);
-    reverbRoomDial.setColour(Slider::ColourIds::textBoxTextColourId, Colours::black);
-    reverbRoomDial.setColour(Slider::ColourIds::textBoxOutlineColourId, Colour());
-    reverbRoomDial.setSliderStyle(Slider::SliderStyle::Rotary);
-    reverbRoomDial.setNumDecimalPlacesToDisplay(0);
-    reverbRoomDial.setSize(150, 100);
-    reverbRoomDial.setTextValueSuffix("%");
+    reverbRoomDial.setTextBoxStyle(Slider::NoTextBox, false, 0, 0);
+    reverbRoomDial.setSliderStyle(Slider::SliderStyle::RotaryVerticalDrag);
+    reverbRoomDial.setSize(80, 80);
     
     // dont change these two
     reverbRoomDial.addListener(this);
     addAndMakeVisible(reverbRoomDial);
     
-    reverbDampDial.setTextBoxStyle(Slider::TextEntryBoxPosition::TextBoxAbove, true, 55, 30);
-    reverbDampDial.setColour(Slider::ColourIds::textBoxTextColourId, Colours::black);
-    reverbDampDial.setColour(Slider::ColourIds::textBoxOutlineColourId, Colour());
-    reverbDampDial.setSliderStyle(Slider::SliderStyle::Rotary);
-    reverbDampDial.setNumDecimalPlacesToDisplay(0);
-    reverbDampDial.setSize(150, 100);
-    reverbDampDial.setTextValueSuffix("%");
-    
+    reverbDampDial.setTextBoxStyle(Slider::NoTextBox, false, 0, 0);
+    reverbDampDial.setSliderStyle(Slider::SliderStyle::RotaryVerticalDrag);
+    reverbDampDial.setSize(80, 80);
+
     // dont change these two
     reverbDampDial.addListener(this);
     addAndMakeVisible(reverbDampDial);
     
-    reverbWidthDial.setTextBoxStyle(Slider::TextEntryBoxPosition::TextBoxAbove, true, 55, 30);
-    reverbWidthDial.setColour(Slider::ColourIds::textBoxTextColourId, Colours::black);
-    reverbWidthDial.setColour(Slider::ColourIds::textBoxOutlineColourId, Colour());
-    reverbWidthDial.setSliderStyle(Slider::SliderStyle::Rotary);
-    reverbWidthDial.setNumDecimalPlacesToDisplay(0);
-    reverbWidthDial.setSize(150, 100);
-    reverbWidthDial.setTextValueSuffix("%");
+    reverbWidthDial.setTextBoxStyle(Slider::NoTextBox, false, 0, 0);
+	reverbWidthDial.setSize(80, 80);
+	reverbWidthDial.setSliderStyle(Slider::SliderStyle::RotaryVerticalDrag);
     
-    // dont change these two
+	// dont change these two
     reverbWidthDial.addListener(this);
     addAndMakeVisible(reverbWidthDial);
     
@@ -143,19 +88,11 @@ TokyoRe_verbAudioProcessorEditor::TokyoRe_verbAudioProcessorEditor (TokyoRe_verb
     reverbRoomValue = new AudioProcessorValueTreeState::SliderAttachment (processor.mState, "room", reverbRoomDial);
     reverbDampValue = new AudioProcessorValueTreeState::SliderAttachment (processor.mState, "damp", reverbDampDial);
     reverbWidthValue = new AudioProcessorValueTreeState::SliderAttachment (processor.mState, "width", reverbWidthDial);
-    
-    //reverbDryDial.setLookAndFeel(&otherLookAndFeel3);
-    //reverbWetDial.setLookAndFeel(&otherLookAndFeel3);
-    reverbMixDial.setLookAndFeel(&otherLookAndFeel3);
-    reverbRoomDial.setLookAndFeel(&otherLookAndFeel2);
-    reverbDampDial.setLookAndFeel(&otherLookAndFeel2);
-    reverbWidthDial.setLookAndFeel(&otherLookAndFeel2);
-    
-    //addAndMakeVisible(reverbDryLabel);
-    //reverbDryLabel.attachToComponent(&reverbDryDial, false);
-    
-    //addAndMakeVisible(reverbWetLabel);
-    //reverbWetLabel.attachToComponent(&reverbWetDial, false);
+
+    reverbMixDial.setLookAndFeel(&otherLookAndFeel);
+    reverbRoomDial.setLookAndFeel(&otherLookAndFeel);
+    reverbDampDial.setLookAndFeel(&otherLookAndFeel);
+    reverbWidthDial.setLookAndFeel(&otherLookAndFeel);
     
     addAndMakeVisible(reverbMixLabel);
     reverbMixLabel.attachToComponent(&reverbMixDial, false);
@@ -169,75 +106,7 @@ TokyoRe_verbAudioProcessorEditor::TokyoRe_verbAudioProcessorEditor (TokyoRe_verb
     addAndMakeVisible(reverbWidthLabel);
     reverbWidthLabel.attachToComponent(&reverbWidthDial, false);
     
-    
-    /*
-    distDriveDial.setSliderStyle(Slider::SliderStyle::Rotary);
-    distDriveDial.setSize(150, 100);
-    //distDriveDial.setRange(20.0, 20000.0);
-    distDriveDial.setTextValueSuffix("%");
-    //distDriveDial.setColour(Slider::ColourIds::rotarySliderFillColourId, Colours::yellowgreen);
-    
-    // dont change these two
-    distDriveDial.addListener(this);
-    addAndMakeVisible(distDriveDial);
-    
-    distRangeDial.setSliderStyle(Slider::SliderStyle::Rotary);
-    distRangeDial.setTextBoxStyle(Slider::TextEntryBoxPosition::TextBoxAbove, true, 55, 30);
-    distRangeDial.setColour(Slider::ColourIds::textBoxTextColourId, Colours::black);
-    distRangeDial.setColour(Slider::ColourIds::textBoxOutlineColourId, Colour());
-    
-    distRangeDial.setSize(150, 100);
-    //distRangeDial.setRange(20.0, 20000.0);
-    distRangeDial.setTextValueSuffix("f");
-    //distRangeDial.setColour(Slider::ColourIds::rotarySliderFillColourId, Colours::yellowgreen);
-    
-    // dont change these two
-    distRangeDial.addListener(this);
-    addAndMakeVisible(distRangeDial);
-    
-    distBlendDial.setSliderStyle(Slider::SliderStyle::Rotary);
-    distBlendDial.setTextBoxStyle(Slider::TextEntryBoxPosition::TextBoxAbove, true, 55, 30);
-    distBlendDial.setColour(Slider::ColourIds::textBoxTextColourId, Colours::black);
-    distBlendDial.setColour(Slider::ColourIds::textBoxOutlineColourId, Colour());
-    distBlendDial.setNumDecimalPlacesToDisplay(2);
-    distBlendDial.setSize(150, 100);
-    //distBlendDial.setRange(20.0, 20000.0);
-    distBlendDial.setTextValueSuffix("%");
-    
-    //distBlendDial.setColour(Slider::ColourIds::rotarySliderFillColourId, Colours::yellowgreen);
-    
-    // dont change these two
-    distBlendDial.addListener(this);
-    addAndMakeVisible(distBlendDial);
-    
-    distVolumeDial.setSliderStyle(Slider::SliderStyle::RotaryHorizontalVerticalDrag);
-    distVolumeDial.setTextBoxStyle(Slider::TextEntryBoxPosition::TextBoxBelow, true, 100, 30);
-    distVolumeDial.setColour(Slider::ColourIds::textBoxTextColourId, Colours::black);
-    distVolumeDial.setColour(Slider::ColourIds::textBoxOutlineColourId, Colour());
-    distVolumeDial.setSize(100, 70);
-    //distVolumeDial.setRange(20.0, 20000.0);
-    distVolumeDial.setTextValueSuffix("%");
-    //distVolumeDial.setColour(Slider::ColourIds::rotarySliderFillColourId, Colours::yellowgreen);
-    
-    // dont change these two
-    distVolumeDial.addListener(this);
-    addAndMakeVisible(distVolumeDial);
-    
-    distDriveDial.setTextBoxStyle(Slider::TextEntryBoxPosition::TextBoxAbove, true, 55, 30);
-    distDriveDial.setColour(Slider::ColourIds::textBoxTextColourId, Colours::black);
-    distDriveDial.setColour(Slider::ColourIds::textBoxOutlineColourId, Colour());
-    distDriveDial.setLookAndFeel(&otherLookAndFeel);
-    distRangeDial.setLookAndFeel(&otherLookAndFeel);
-    distBlendDial.setLookAndFeel(&otherLookAndFeel);
-    distVolumeDial.setLookAndFeel(&otherLookAndFeel3);
-    
-    
-    // dont change these four
-    distDriveValue = new AudioProcessorValueTreeState::SliderAttachment (processor.mState, "drive", distDriveDial);
-    distRangeValue = new AudioProcessorValueTreeState::SliderAttachment (processor.mState, "range", distRangeDial);
-    distBlendValue = new AudioProcessorValueTreeState::SliderAttachment (processor.mState, "blend", distBlendDial);
-    distVolumeValue = new AudioProcessorValueTreeState::SliderAttachment (processor.mState, "volume", distVolumeDial);
-     */
+
     
 }
 
@@ -248,44 +117,24 @@ TokyoRe_verbAudioProcessorEditor::~TokyoRe_verbAudioProcessorEditor()
 //==============================================================================
 void TokyoRe_verbAudioProcessorEditor::paint (Graphics& g)
 {
-    // (Our component is opaque, so we must completely fill the background with a solid colour)
+    // (Our plugin background is opaque, so we must completely fill the background with a solid colour or image)
     
-    /*Image background = ImageCache::getFromMemory(BinaryData::ayaya7_png, BinaryData::ayaya7_pngSize);
-    g.drawImageAt(background, 0, 0);*/
-    
-    g.fillAll (Colours::darkorchid);
-    
-    // g.setColour (Colours::aqua);
-    //g.setFont (15.0f);
-    // g.drawFittedText ("GOVERSA2 PLUGIN", getLocalBounds(), Justification::centred, 1);
-    
-    g.setColour(Colours::black);
-    g.setFont(Font ("Arial", 20.0f, Font::italic));
-    //g.drawFittedText("Random Test Font! Hi There!", 10, 10, getWidth(), 50,
-    //
+    Image background = ImageCache::getFromMemory(BinaryData::KanekiWing_png, BinaryData::KanekiWing_pngSize);
+    g.drawImageAt(background, 0, 0);
+ 
 }
 
 void TokyoRe_verbAudioProcessorEditor::resized()
 {
-    // This is generally where you'll want to lay out the positions of any
-    // subcomponents in your editor..
-    
-    //mixSlider.setBounds(10, getHeight()-100, 80, 80);
-    filterCutoffDial.setBounds(0, getHeight()-130, 80, 80);
-    filterResDial.setBounds(100, getHeight()-130, 80, 80);
-    
-    /*distDriveDial.setBounds(200, getHeight()-130, 80, 80);
-    distRangeDial.setBounds(300, getHeight()-130, 80, 80);
-    distBlendDial.setBounds(400, getHeight()-130, 80, 80);
-    distVolumeDial.setBounds(425, getHeight()-310, 80, 80);*/
-    
-    //reverbDryDial.setBounds(0, getHeight()-280, 80, 80);
-    //reverbWetDial.setBounds(100, getHeight()-280, 80, 80);
-    reverbMixDial.setBounds(0, getHeight()-280, 80, 80);
-    reverbRoomDial.setBounds(0, getHeight()-450, 80, 80);
-    reverbDampDial.setBounds(100, getHeight()-450, 80, 80);
-    reverbWidthDial.setBounds(150, getHeight()-450, 80, 80);
-    
+	//area where you progam the position of the sliders 
+
+	filterCutoffDial.setBounds(53, getHeight() - 113, 80, 80);
+	filterResDial.setBounds(158, getHeight() - 113, 80, 80);
+  
+	reverbWidthDial.setBounds(258, getHeight() - 113, 80, 80);
+    reverbRoomDial.setBounds(358, getHeight()-113, 80, 80);
+    reverbDampDial.setBounds(458, getHeight()-113, 80, 80);
+	reverbMixDial.setBounds(568, getHeight() - 113, 80, 80);
 }
 
 void TokyoRe_verbAudioProcessorEditor::sliderValueChanged(Slider* slider)
--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -20,188 +20,102 @@
 class OtherLookAndFeel : public LookAndFeel_V4
 {
 public:
-    
-    void drawRotarySlider (Graphics &g, int x, int y, int width, int height, float sliderPos, float rotaryStartAngle, float rotaryEndAngle, Slider &slider) override
-    {
-        float diameter = jmin(width, height);
-        float radius = diameter / 2;
-        float centreX = x + width / 2;
-        float centreY = y + height / 2;
-        float rx = centreX - radius;
-        float ry = centreY - radius;
-        float angle = rotaryStartAngle + (sliderPos * (rotaryEndAngle - rotaryStartAngle));
-        
-        std::cout << rotaryStartAngle << " " << rotaryEndAngle << std::endl;
-        
-        Rectangle<float> dialArea (rx, ry, diameter, diameter);
-        
-        g.setColour(Colours::red);
-        //g.drawRect(dialArea);
-        g.fillEllipse(dialArea);
-        
-        g.setColour(Colours::black);
-        //g.fillEllipse(centreX, centreY, 5, 5);
-        
-        Path dialTick;
-        dialTick.addRectangle(-5.0f, -radius, 10.0f, radius * 0.33);
-        
-        g.fillPath(dialTick, AffineTransform::rotation(angle).translated(centreX, centreY));
-        
-        g.setColour(Colours::black);
-        g.drawEllipse(rx, ry, diameter, diameter, 5.0f);
-        
-    }
-    
+	
+	Image img1 = ImageCache::getFromMemory(BinaryData::PurpleAnimate_png, BinaryData::PurpleAnimate_pngSize);
+	
+	void drawRotarySlider(Graphics &g, int x, int y, int width, int height, float sliderPos, float rotaryStartAngle, float rotaryEndAngle, Slider &slider) override
+	{
+		if (img1.isValid())
+		{
+			const double rotation = (slider.getValue()
+				- slider.getMinimum())
+				/ (slider.getMaximum()
+					- slider.getMinimum());
+
+			const int frames = img1.getHeight() / img1.getWidth();
+			const int frameId = (int)ceil(rotation * ((double)frames - 1.0));
+			const float radius = jmin(width / 2.0f, height / 2.0f);
+			const float centerX = x + width * 0.5f;
+			const float centerY = y + height * 0.5f;
+			const float rx = centerX - radius - 1.0f;
+			const float ry = centerY - radius;
+
+			g.drawImage(img1,
+				(int)rx,
+				(int)ry,
+				2 * (int)radius,
+				2 * (int)radius,
+				0,
+				frameId*img1.getWidth(),
+				img1.getWidth(),
+				img1.getWidth());
+		}
+		
+		
+		
+	}
+		
 };
 
-class OtherLookAndFeel2 : public LookAndFeel_V4
-{
-public:
-    
-    void drawRotarySlider (Graphics &g, int x, int y, int width, int height, float sliderPos, float rotaryStartAngle, float rotaryEndAngle, Slider &slider) override
-    {
-        float diameter = jmin(width, height);
-        float radius = diameter / 2;
-        float centreX = x + width / 2;
-        float centreY = y + height / 2;
-        float rx = centreX - radius;
-        float ry = centreY - radius;
-        float angle = rotaryStartAngle + (sliderPos * (rotaryEndAngle - rotaryStartAngle));
-        
-        std::cout << rotaryStartAngle << " " << rotaryEndAngle << std::endl;
-        
-        Rectangle<float> dialArea (rx, ry, diameter, diameter);
-        
-        g.setColour(Colours::yellowgreen);
-        //g.drawRect(dialArea);
-        g.fillEllipse(dialArea);
-        
-        g.setColour(Colours::black);
-        //g.fillEllipse(centreX, centreY, 5, 5);
-        
-        Path dialTick;
-        dialTick.addRectangle(-5.0f, -radius, 10.0f, radius * 0.33);
-        
-        g.fillPath(dialTick, AffineTransform::rotation(angle).translated(centreX, centreY));
-        
-        g.setColour(Colours::black);
-        g.drawEllipse(rx, ry, diameter, diameter, 5.0f);
-        
-    }
-    
-};
+	class TokyoRe_verbAudioProcessorEditor : public AudioProcessorEditor, Slider::Listener
+	{
+	public:
+		TokyoRe_verbAudioProcessorEditor(TokyoRe_verbAudioProcessor&);
+		~TokyoRe_verbAudioProcessorEditor();
 
-class OtherLookAndFeel3 : public LookAndFeel_V4
-{
-public:
-    
-    void drawRotarySlider (Graphics &g, int x, int y, int width, int height, float sliderPos, float rotaryStartAngle, float rotaryEndAngle, Slider &slider) override
-    {
-        float diameter = jmin(width, height);
-        float radius = diameter / 2;
-        float centreX = x + width / 2;
-        float centreY = y + height / 2;
-        float rx = centreX - radius;
-        float ry = centreY - radius;
-        float angle = rotaryStartAngle + (sliderPos * (rotaryEndAngle - rotaryStartAngle));
-        
-        std::cout << rotaryStartAngle << " " << rotaryEndAngle << std::endl;
-        
-        Rectangle<float> dialArea (rx, ry, diameter, diameter);
-        
-        g.setColour(Colours::orangered);
-        //g.drawRect(dialArea);
-        g.fillEllipse(dialArea);
-        
-        g.setColour(Colours::black);
-        //g.fillEllipse(centreX, centreY, 5, 5);
-        
-        Path dialTick;
-        dialTick.addRectangle(-3.0f, -radius, 6.0f, radius * 0.33);
-        
-        g.fillPath(dialTick, AffineTransform::rotation(angle).translated(centreX, centreY));
-        
-        g.setColour(Colours::black);
-        g.drawEllipse(rx, ry, diameter, diameter, 5.0f);
-        
-    }
-    
-};
+		//==============================================================================
+		void paint(Graphics&) override;
+		void resized() override;
 
-class TokyoRe_verbAudioProcessorEditor  : public AudioProcessorEditor, Slider::Listener
-{
-public:
-    TokyoRe_verbAudioProcessorEditor (TokyoRe_verbAudioProcessor&);
-    ~TokyoRe_verbAudioProcessorEditor();
+	private:
 
-    //==============================================================================
-    void paint (Graphics&) override;
-    void resized() override;
+		juce::Image backGround;
 
-private:
-    
-    juce::Image backGround;
-    
-    OtherLookAndFeel otherLookAndFeel;
-    OtherLookAndFeel2 otherLookAndFeel2;
-    OtherLookAndFeel3 otherLookAndFeel3;
-    
-    Slider filterCutoffDial;
-    Slider filterResDial;
-    ScopedPointer<AudioProcessorValueTreeState::SliderAttachment> filterCutoffValue;
-    ScopedPointer<AudioProcessorValueTreeState::SliderAttachment> filterResValue;
-    //std::unique_ptr<AudioProcessorValueTreeState::SliderAttachment> filterCutoffValue;
-    //std::unique_ptr<AudioProcessorValueTreeState::SliderAttachment> filterResValue;
-    
-    
-    Label filterCutoffLabel;
-    Label filterResLabel;
-    
-    //Slider reverbDryDial;
-    //Slider reverbWetDial;
-    Slider reverbMixDial;
-    Slider reverbRoomDial;
-    Slider reverbDampDial;
-    Slider reverbWidthDial;
-    
-    //ScopedPointer<AudioProcessorValueTreeState::SliderAttachment> reverbDryValue;
-    //ScopedPointer<AudioProcessorValueTreeState::SliderAttachment> reverbWetValue;
-    ScopedPointer<AudioProcessorValueTreeState::SliderAttachment> reverbMixValue;
-    ScopedPointer<AudioProcessorValueTreeState::SliderAttachment> reverbRoomValue;
-    ScopedPointer<AudioProcessorValueTreeState::SliderAttachment> reverbDampValue;
-    ScopedPointer<AudioProcessorValueTreeState::SliderAttachment> reverbWidthValue;
-    
-    //Label reverbDryLabel;
-    //Label reverbWetLabel;
-    Label reverbMixLabel;
-    Label reverbRoomLabel;
-    Label reverbDampLabel;
-    Label reverbWidthLabel;
-    
-    /*Slider distDriveDial;
-    Slider distRangeDial;
-    Slider distBlendDial;
-    Slider distVolumeDial;
-    ScopedPointer<AudioProcessorValueTreeState::SliderAttachment> distDriveValue;
-    ScopedPointer<AudioProcessorValueTreeState::SliderAttachment> distRangeValue;
-    ScopedPointer<AudioProcessorValueTreeState::SliderAttachment> distBlendValue;
-    ScopedPointer<AudioProcessorValueTreeState::SliderAttachment> distVolumeValue;
-    
-    Label distDriveLabel;
-    Label distRangeLabel;
-    Label distBlendLabel;
-    Label distVolumeLabel;*/
-    
-    Slider mixSlider;
-    Label mixLabel;
-    
-    std::unique_ptr<AudioProcessorValueTreeState::SliderAttachment> mixAttachment;
-    
-    void sliderValueChanged(Slider * slider) override;
-    
-    // This reference is provided as a quick way for your editor to
-    // access the processor object that created it.
-    TokyoRe_verbAudioProcessor& processor;
+		OtherLookAndFeel otherLookAndFeel;
 
-    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (TokyoRe_verbAudioProcessorEditor)
-};
+
+		Slider filterCutoffDial;
+		Slider filterResDial;
+		ScopedPointer<AudioProcessorValueTreeState::SliderAttachment> filterCutoffValue;
+		ScopedPointer<AudioProcessorValueTreeState::SliderAttachment> filterResValue;
+		//std::unique_ptr<AudioProcessorValueTreeState::SliderAttachment> filterCutoffValue;
+		//std::unique_ptr<AudioProcessorValueTreeState::SliderAttachment> filterResValue;
+
+
+		Label filterCutoffLabel;
+		Label filterResLabel;
+
+		//Slider reverbDryDial;
+		//Slider reverbWetDial;
+		Slider reverbMixDial;
+		Slider reverbRoomDial;
+		Slider reverbDampDial;
+		Slider reverbWidthDial;
+
+		ScopedPointer<AudioProcessorValueTreeState::SliderAttachment> reverbMixValue;
+		ScopedPointer<AudioProcessorValueTreeState::SliderAttachment> reverbRoomValue;
+		ScopedPointer<AudioProcessorValueTreeState::SliderAttachment> reverbDampValue;
+		ScopedPointer<AudioProcessorValueTreeState::SliderAttachment> reverbWidthValue;
+
+		//Label reverbDryLabel;
+		//Label reverbWetLabel;
+		Label reverbMixLabel;
+		Label reverbRoomLabel;
+		Label reverbDampLabel;
+		Label reverbWidthLabel;
+
+
+
+		Slider mixSlider;
+		Label mixLabel;
+
+		std::unique_ptr<AudioProcessorValueTreeState::SliderAttachment> mixAttachment;
+
+		void sliderValueChanged(Slider * slider) override;
+
+		// This reference is provided as a quick way for your editor to
+		// access the processor object that created it.
+		TokyoRe_verbAudioProcessor& processor;
+
+		JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(TokyoRe_verbAudioProcessorEditor)
+	};
